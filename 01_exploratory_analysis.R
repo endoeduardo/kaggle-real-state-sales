133da2ad69167854ecd53291a62d5ee60681db36
endoeduardo <eduardo.t.endo@gmail.com>
2023-12-13 22:24:42 -0300

    feat: initial eda exploring the property data type

--- 01_exploratory_analysis.R
+++ 01_exploratory_analysis.R
@@ -8,5 +8,27 @@ setwd("C:/Users/Eduardo Endo/Documents/r_scripts/git_projects/kaggle-real-state-
 real_state_sales <- read_csv("dataset/real-state-sales.csv") %>% 
   janitor::clean_names()
 
-dummy <- real_state_sales %>% 
-  count(list_year)
+
+real_state_sales %>% 
+  count(town)
+
+
+#This part shows that the property type categorization has changed along the years
+#an maybe the property_type is level 1 and the residential_type is level 2
+df_plot <- real_state_sales %>% 
+  # filter(property_type == 'Commercial') %>% 
+  mutate(list_year = as.factor(list_year)) %>% 
+  group_by(list_year, property_type) %>% 
+  summarise(total = n()) %>% 
+  ungroup()
+  
+  
+ggplot(df_plot, aes(x = total, y = list_year)) +
+  geom_col() + 
+  facet_wrap(.~property_type) +
+  theme_bw()
+
+ggsave('charts/eda/property_type_changes_through_the_years.png', 
+       height = 15,
+       width = 15,
+       units = 'cm')
